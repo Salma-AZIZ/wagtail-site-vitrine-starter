5aad1d3c647c1f54b52c3b758656b4d26e818830
Salma AZIZ <aziz.salma095@gmail.com>
2025-11-27 20:06:53 +0100

    Wagtail Starter Kit - V1

---
+++ sitedemo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-85ma8%6$dz!ha8(gbb0tqn3)kz@s@j7fs*k&x@**vzh^w3hd%o"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
